e1eb863f31969416d5b7aabb4f53dbb5f3736713
Claudio Romero <romerolrocha@gmail.com>
2020-02-21 19:31:46 -0300

    Dependency Injection

---
+++ di/di.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	err := http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
